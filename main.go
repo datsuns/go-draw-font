aaa4878e023afe249e3725595f1db54b62141025
s-bando <satoshi_bando@solver.co.jp>
2019-10-09 11:06:20 +0900

    add config

--- main.go
+++ main.go
@@ -22,11 +22,76 @@ import (
 	"golang.org/x/image/font"
 	//"golang.org/x/image/font/gofont/gobold"
 	"golang.org/x/image/math/fixed"
+	"gopkg.in/yaml.v2"
 )
 
-func load_font() *truetype.Font {
+var (
+	DefaultXPos = []fixed.Int26_6{
+		fixed.I(0),
+		fixed.I(150 - 7),
+		fixed.I(300 - 14),
+		fixed.I(450 - 21),
+		fixed.I(600 - 28),
+		fixed.I(750 - 35),
+		fixed.I(900 - 42),
+	}
+	DefaultYPos = []fixed.Int26_6{
+		fixed.I(90),
+		fixed.I(235),
+		fixed.I(380 + 2),
+		fixed.I(525 + 2),
+		fixed.I(670 + 2),
+	}
+)
+
+type Config struct {
+	Output struct {
+		Year  int
+		Month int
+	}
+	Font  string
+	Size  float64
+	Image struct {
+		Width  int `yaml:"width"`
+		Height int `yaml:"height"`
+	}
+	XPos []int `yaml:"XPos,flow"`
+	YPos []int `yaml:"YPos,flow"`
+}
+
+func (c *Config) Dump() {
+	fmt.Printf("output : %v-%v\n", c.Output.Year, c.Output.Month)
+	fmt.Printf("font file : %v\n", c.Font)
+	fmt.Printf("font size : %v\n", c.Size)
+	fmt.Printf("    image : %vx%v\n", c.Image.Width, c.Image.Height)
+	fmt.Printf("    Xpos  : ")
+	for i, p := range c.XPos {
+		fmt.Printf("[%v:%v],", i, p)
+	}
+	fmt.Printf("\n")
+	fmt.Printf("    Ypos  : ")
+	for i, p := range c.YPos {
+		fmt.Printf("[%v:%v],", i, p)
+	}
+	fmt.Printf("\n")
+}
+
+func load_config(path string) (*Config, error) {
+	ret := Config{}
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(buf, &ret)
+	if err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
+func load_font(path string) *truetype.Font {
 	// フォントファイルを読み込み
-	ftBinary, err := ioutil.ReadFile("GN-KillGothic-U-KanaNA.ttf")
+	ftBinary, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -41,11 +106,14 @@ func load_font() *truetype.Font {
 }
 
 func main() {
-	ft := load_font()
-
+	cfg, err := load_config("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	cfg.Dump()
+	ft := load_font(cfg.Font)
 	opt := truetype.Options{
-		//Size:              60,
-		Size:              30,
+		Size:              cfg.Size,
 		DPI:               0,
 		Hinting:           0,
 		GlyphCacheEntries: 0,
@@ -53,8 +121,8 @@ func main() {
 		SubPixelsY:        0,
 	}
 
-	imageWidth := 1500
-	imageHeight := 1000
+	imageWidth := cfg.Image.Width
+	imageHeight := cfg.Image.Height
 
 	img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
 	face := truetype.NewFace(ft, &opt)
@@ -73,22 +141,6 @@ func main() {
 		"20", "21", "22", "23", "24", "25", "26",
 		"27", "28", "29", "30", "31",
 	}
-	xpos := []fixed.Int26_6{
-		fixed.I(0),
-		fixed.I(150 - 7),
-		fixed.I(300 - 14),
-		fixed.I(450 - 21),
-		fixed.I(600 - 28),
-		fixed.I(750 - 35),
-		fixed.I(900 - 42),
-	}
-	hpos := []fixed.Int26_6{
-		fixed.I(90),
-		fixed.I(235),
-		fixed.I(380 + 2),
-		fixed.I(525 + 2),
-		fixed.I(670 + 2),
-	}
 
 	file, err := os.Create(`test.png`)
 	if err != nil {
@@ -103,8 +155,8 @@ func main() {
 		if c == "." {
 			continue
 		}
-		dr.Dot.X = xpos[i%7]
-		dr.Dot.Y = hpos[h_idx]
+		dr.Dot.X = fixed.I(cfg.XPos[i%7])
+		dr.Dot.Y = fixed.I(cfg.YPos[h_idx])
 		buf.Reset()
 		fmt.Printf("%2v) x:%v, y:%v char[%v]\n", i, dr.Dot.X, dr.Dot.Y, c)
 		dr.DrawString(c)
